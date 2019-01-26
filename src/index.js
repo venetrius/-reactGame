44bb73616b4060235430fc76a4a3cb15ca008576
Venetrius <venetrius@gmail.com>
2019-01-26 11:37:04 -0700

    fixing an issue where one cell can be clicked multiple times, and or after a winner is declered

--- src/index.js
+++ src/index.js
@@ -44,7 +44,10 @@ class Board extends React.Component {
   }
 	
   handleClick(i) {
-    const squares = this.state.squares.slice();
+	const squares = this.state.squares.slice();
+	if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({squares: squares,
 		xIsNext: !this.state.xIsNext,
