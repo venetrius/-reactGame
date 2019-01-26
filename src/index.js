511d557d59b6a0527a7e8604688b66a8eff8e90e
Venetrius <venetrius@gmail.com>
2019-01-26 14:09:51 -0700

    additional task: Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/index.js
+++ src/index.js
@@ -43,9 +43,46 @@ class Board extends React.Component {
     );
   }
 
+
+
+
+  static genSqures(){
+    var toAdd = document.createDocumentFragment();
+    for(var i=0; i < 3; i++){
+       var newDiv = document.createElement('div');
+       newDiv.className = 'board-row';
+
+       toAdd.appendChild(newDiv);
+    }
+    
+    document.appendChild(toAdd);
+  }
+
+
+  createTable(){
+    let table = []
+
+    // Outer loop to create parent
+    for (let i = 0; i < 3; i++) {
+      let children = []
+      //Inner loop to create children
+      for (let j = 0; j < 3; j++) {
+        children.push(this.renderSquare(i +j *3))
+      }
+      //Create the parent and add the children
+      table.push(<div>{children}</div>)
+    }
+    return table
+    
+}
+
   render() {
     return (
       <div>
+        {this.createTable()}
+      </div>
+      
+      /*<div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -62,6 +99,7 @@ class Board extends React.Component {
           {this.renderSquare(8)}
         </div>
       </div>
+*/
     );
   }
 }
